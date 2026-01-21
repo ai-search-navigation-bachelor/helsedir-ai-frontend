3aa09c709febb791cb8b0fd8ec05a2b516044bdc
elling <elling.bakaas@gmail.com>
2026-01-21 10:01:58 +0100

    La til custom fargetema

---
+++ design-tokens-build/colors.d.ts
@@ -0,0 +1,32 @@
+/* This file is deprecated and will be removed in a future release. Use types.d.ts instead */
+/* build: v1.11.0 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    'logobla-1': never;
+    'logobla-2': never;
+    hvit: never;
+    'bla-1': never;
+    'bla-2': never;
+    'bla-3': never;
+    'gronn-1': never;
+    'gronn-2': never;
+    'gronn-3': never;
+    'gul-1': never;
+    'gul-2': never;
+    'gul-3': never;
+    'lilla-1': never;
+    'lilla-2': never;
+    'lilla-3': never;
+    svart: never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
